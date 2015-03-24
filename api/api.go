77b363532f844adcfedc0bfd099c3bee5364d0db
Sean Schulte <sirsean@gmail.com>
2015-03-24 13:22:49 -0500

    go fmt + config file as argument

--- api/api.go
+++ api/api.go
@@ -2,14 +2,14 @@ package api
 
 import (
 	"encoding/json"
+	"github.com/alliewas/cta-near-me/arrival"
+	"github.com/alliewas/cta-near-me/station"
 	"log"
 	"net/http"
-    "sort"
-    "strconv"
-    "strings"
-    "sync"
-    "github.com/alliewas/cta-near-me/station"
-    "github.com/alliewas/cta-near-me/arrival"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 func Nearby(w http.ResponseWriter, r *http.Request) {
@@ -17,142 +17,142 @@ func Nearby(w http.ResponseWriter, r *http.Request) {
 	long, _ := strconv.ParseFloat(r.URL.Query().Get("longitude"), 64)
 	log.Printf("nearby lat %v long %v", lat, long)
 
-    nearbyStations := station.StationsNear(lat, long)
-    loadArrivals(nearbyStations)
-    log.Printf("stations: %v", nearbyStations)
+	nearbyStations := station.StationsNear(lat, long)
+	loadArrivals(nearbyStations)
+	log.Printf("stations: %v", nearbyStations)
 
 	response, _ := json.Marshal(nearbyStations)
 	w.Write(response)
 }
 
 func Lines(w http.ResponseWriter, r *http.Request) {
-    response, _ := json.Marshal(station.Lines)
-    w.Write(response)
+	response, _ := json.Marshal(station.Lines)
+	w.Write(response)
 }
 
 func Stations(w http.ResponseWriter, r *http.Request) {
-    lineKey := r.URL.Query().Get("line")
+	lineKey := r.URL.Query().Get("line")
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("latitude"), 64)
 	long, _ := strconv.ParseFloat(r.URL.Query().Get("longitude"), 64)
 
-    stations := station.StationsForLine(lineKey, lat, long)
+	stations := station.StationsForLine(lineKey, lat, long)
 
-    response, _ := json.Marshal(stations)
-    w.Write(response)
+	response, _ := json.Marshal(stations)
+	w.Write(response)
 }
 
 func Station(w http.ResponseWriter, r *http.Request) {
-    log.Printf("loading station")
-    stationId, _ := strconv.ParseInt(r.URL.Query().Get("stationId"), 10, 64)
+	log.Printf("loading station")
+	stationId, _ := strconv.ParseInt(r.URL.Query().Get("stationId"), 10, 64)
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("latitude"), 64)
 	long, _ := strconv.ParseFloat(r.URL.Query().Get("longitude"), 64)
-    station := station.GetStation(int(stationId), lat, long)
+	station := station.GetStation(int(stationId), lat, long)
 
-    var wg sync.WaitGroup
-    loadStationArrivals(station, &wg)
-    wg.Wait()
+	var wg sync.WaitGroup
+	loadStationArrivals(station, &wg)
+	wg.Wait()
 
-    response, _ := json.Marshal(station)
-    w.Write(response)
+	response, _ := json.Marshal(station)
+	w.Write(response)
 }
 
 func Stops(w http.ResponseWriter, r *http.Request) {
-    log.Printf("loading stops")
-    stopIdStrings := strings.Split(r.URL.Query().Get("stopIds"), ",")
+	log.Printf("loading stops")
+	stopIdStrings := strings.Split(r.URL.Query().Get("stopIds"), ",")
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("latitude"), 64)
 	long, _ := strconv.ParseFloat(r.URL.Query().Get("longitude"), 64)
 
-    stopIds := make([]int, len(stopIdStrings))
-    for i, stopIdString := range stopIdStrings {
-        stopId, _ := strconv.ParseInt(stopIdString, 10, 64)
-        stopIds[i] = int(stopId)
-    }
+	stopIds := make([]int, len(stopIdStrings))
+	for i, stopIdString := range stopIdStrings {
+		stopId, _ := strconv.ParseInt(stopIdString, 10, 64)
+		stopIds[i] = int(stopId)
+	}
 
-    var wg sync.WaitGroup
-    wg.Add(len(stopIds))
-    stopEtas := NewLoadedStopArrivals()
-    for _, stopId := range stopIds {
-        go func(stopId int, stopEtas loadedStopArrivals, wg *sync.WaitGroup) {
-            a, _ := arrival.Load(stopId)
-            stopEtas.Set(stopId, a)
-            wg.Done()
-        }(stopId, stopEtas, &wg)
-    }
-    wg.Wait()
+	var wg sync.WaitGroup
+	wg.Add(len(stopIds))
+	stopEtas := NewLoadedStopArrivals()
+	for _, stopId := range stopIds {
+		go func(stopId int, stopEtas loadedStopArrivals, wg *sync.WaitGroup) {
+			a, _ := arrival.Load(stopId)
+			stopEtas.Set(stopId, a)
+			wg.Done()
+		}(stopId, stopEtas, &wg)
+	}
+	wg.Wait()
 
-    stationStops := make(map[int]map[int]bool)
-    for _, etas := range stopEtas.All() {
-        for _, eta := range etas {
-            if _, ok := stationStops[eta.StationId]; !ok {
-                stationStops[eta.StationId] = make(map[int]bool)
-            }
-            stationStops[eta.StationId][eta.StopId] = true
-        }
-    }
-    log.Printf("station stops: %v", stationStops)
+	stationStops := make(map[int]map[int]bool)
+	for _, etas := range stopEtas.All() {
+		for _, eta := range etas {
+			if _, ok := stationStops[eta.StationId]; !ok {
+				stationStops[eta.StationId] = make(map[int]bool)
+			}
+			stationStops[eta.StationId][eta.StopId] = true
+		}
+	}
+	log.Printf("station stops: %v", stationStops)
 
-    stations := make([]station.StationWrapper, 0, len(stationStops))
-    for stationId, stopIdMap := range stationStops {
-        s := station.GetStation(stationId, lat, long)
-        for _, stop := range s.StopArrivals {
-            if stopIdMap[stop.StopId] {
-                stop.Arrivals = stopEtas.Get(stop.StopId)
-            }
-        }
-        stations = append(stations, s)
-    }
+	stations := make([]station.StationWrapper, 0, len(stationStops))
+	for stationId, stopIdMap := range stationStops {
+		s := station.GetStation(stationId, lat, long)
+		for _, stop := range s.StopArrivals {
+			if stopIdMap[stop.StopId] {
+				stop.Arrivals = stopEtas.Get(stop.StopId)
+			}
+		}
+		stations = append(stations, s)
+	}
 
-    sorter := station.NewStationSorter(stations)
-    sort.Sort(sorter)
+	sorter := station.NewStationSorter(stations)
+	sort.Sort(sorter)
 
-    response, _ := json.Marshal(stations)
-    w.Write(response)
+	response, _ := json.Marshal(stations)
+	w.Write(response)
 }
 
 type loadedStopArrivals struct {
-    sync.RWMutex
-    data map[int][]arrival.Eta
+	sync.RWMutex
+	data map[int][]arrival.Eta
 }
 
 func NewLoadedStopArrivals() loadedStopArrivals {
-    c := loadedStopArrivals{}
-    c.data = make(map[int][]arrival.Eta)
-    return c
+	c := loadedStopArrivals{}
+	c.data = make(map[int][]arrival.Eta)
+	return c
 }
 
 func (c loadedStopArrivals) Get(stopId int) []arrival.Eta {
-    c.Lock()
-    defer c.Unlock()
-    return c.data[stopId]
+	c.Lock()
+	defer c.Unlock()
+	return c.data[stopId]
 }
 
 func (c loadedStopArrivals) Set(stopId int, etas []arrival.Eta) {
-    c.Lock()
-    defer c.Unlock()
-    c.data[stopId] = etas
+	c.Lock()
+	defer c.Unlock()
+	c.data[stopId] = etas
 }
 
 func (c loadedStopArrivals) All() map[int][]arrival.Eta {
-    c.Lock()
-    defer c.Unlock()
-    return c.data
+	c.Lock()
+	defer c.Unlock()
+	return c.data
 }
 
 func loadArrivals(stations []station.StationWrapper) {
-    var wg sync.WaitGroup
-    for _, station := range stations {
-        loadStationArrivals(station, &wg)
-    }
-    wg.Wait()
+	var wg sync.WaitGroup
+	for _, station := range stations {
+		loadStationArrivals(station, &wg)
+	}
+	wg.Wait()
 }
 
 func loadStationArrivals(s station.StationWrapper, wg *sync.WaitGroup) {
-    log.Printf("loading station: %v", s.Name)
-    wg.Add(len(s.StopArrivals))
-    for _, stop := range s.StopArrivals {
-        go func(stop *station.StopWrapper, wg *sync.WaitGroup) {
-            stop.Arrivals, _ = arrival.Load(stop.StopId)
-            wg.Done()
-        }(stop, wg)
-    }
+	log.Printf("loading station: %v", s.Name)
+	wg.Add(len(s.StopArrivals))
+	for _, stop := range s.StopArrivals {
+		go func(stop *station.StopWrapper, wg *sync.WaitGroup) {
+			stop.Arrivals, _ = arrival.Load(stop.StopId)
+			wg.Done()
+		}(stop, wg)
+	}
 }
--- config/config.go
+++ config/config.go
@@ -1,36 +1,44 @@
 package config
 
 import (
-    "log"
-    "code.google.com/p/gcfg"
+	"code.google.com/p/gcfg"
+	"log"
+	"os"
 )
 
 type Config struct {
-    CtaApi struct {
-        Key string
-    }
+	CtaApi struct {
+		Key string
+	}
 
-    Host struct {
-        Port string
-        Path string
-    }
+	Host struct {
+		Port string
+		Path string
+	}
 }
 
 var cfg Config
 var loaded bool
 
 func Get() Config {
-    if !loaded {
-        Load()
-    }
-    return cfg
+	if !loaded {
+		Load()
+	}
+	return cfg
 }
 
 func Load() {
-    err := gcfg.ReadFileInto(&cfg, "/etc/cta-near-me/cta-near-me.gcfg")
-    if err != nil {
-        log.Printf("Failed to read config: %v", err)
-    } else {
-        loaded = true
-    }
+	var file string
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	} else {
+		file = "/etc/cta-near-me/cta-near-me.gcfg"
+	}
+	log.Printf("Loading from config file: %v", file)
+	err := gcfg.ReadFileInto(&cfg, file)
+	if err != nil {
+		log.Printf("Failed to read config: %v", err)
+	} else {
+		loaded = true
+	}
 }
